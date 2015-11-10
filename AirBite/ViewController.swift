607d790bb09387ee048aeb2799a25a1a71a7bc18
cordovaorlando <cordovaorlando@hotmail.com>
2015-11-10 10:11:10 -0600

    Building Main StoryBoard and Launch StoryBoard

--- AirBite/ViewController.swift
+++ AirBite/ViewController.swift
@@ -8,17 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var airportField: UITextField!
+    @IBOutlet weak var airlineField: UITextField!
+    @IBOutlet weak var flightField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.airportField.delegate = self;
+        self.airlineField.delegate = self;
+        self.flightField.delegate = self;
     }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    
+    
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        airportField.resignFirstResponder()
+        airlineField.resignFirstResponder()
+        flightField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        airportField.resignFirstResponder()
+        airlineField.resignFirstResponder()
+        flightField.resignFirstResponder()
+        return true
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+
 
 
 }
